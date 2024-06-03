4d58585bb07fdad622241f94947b77ace767ea86
Hung Nguyen <nguyenhungg127@gmail.com>
2024-06-03 13:34:22 +0700

    hello world!

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+// Hello World! in c
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// 'gcc' is not recognized as an internal or external command,
+// operable program or batch file.
+
+// fix
+// Add the path to the 'gcc' executable to the system PATH variable.
+// The path is usually C:\MinGW\bin.
